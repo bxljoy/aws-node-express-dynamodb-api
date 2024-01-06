cd7ac853aa32c6f90a0029c91769cf9ecef6a0fc
bxljoy <259500313@qq.com>
2024-01-06 23:44:29 +0100

    adding updateUser and deleteUser api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Purpose: Lambda function to handle CRUD operations on DynamoDB table
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, PutCommand, DeleteCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import express from "express";
 import serverless from "serverless-http";
 
@@ -12,7 +12,7 @@ const dynamoDbClient = DynamoDBDocumentClient.from(client);
 
 app.use(express.json());
 
-app.get("/users/:userId", async function (req, res) {
+app.get("/getUser/:userId", async function (req, res) {
   const params = {
     TableName: USERS_TABLE,
     Key: {
@@ -36,7 +36,7 @@ app.get("/users/:userId", async function (req, res) {
   }
 });
 
-app.post("/users", async function (req, res) {
+app.post("/addUser", async function (req, res) {
   const { userId, name } = req.body;
   if (typeof userId !== "string") {
     res.status(400).json({ error: '"userId" must be a string' });
@@ -61,11 +61,63 @@ app.post("/users", async function (req, res) {
   }
 });
 
+app.post("/updateUser", async function (req, res) {
+  const { userId, name } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== "string") {
+    res.status(400).json({ error: '"name" must be a string' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: userId,
+    },
+    UpdateExpression: "set #name = :name",
+    ExpressionAttributeNames: {
+      "#name": "name",
+    },
+    ExpressionAttributeValues: {
+      ":name": name,
+    },
+  };
+
+  try {
+    await dynamoDbClient.send(new UpdateCommand(params));
+    res.json({ userId, name });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not update user" });
+  }
+});
+
+app.post("/deleteUser", async function (req, res) {
+  const { userId } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } 
+
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: userId
+    },
+  };
+
+  try {
+    await dynamoDbClient.send(new DeleteCommand(params));
+    res.json({ userId });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not delete user" });
+  }
+});
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
   });
 });
-
 
 export const handler = serverless(app);
